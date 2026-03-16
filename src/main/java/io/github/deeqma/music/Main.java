be8294895c8c54a157107e8af9d4ee96f517bda7
stykle <Maxamadabdulahist@outlook.com>
2026-03-16 11:52:40 +0100

    removed Controller

--- src/main/java/io/github/deeqma/music/Main.java
+++ src/main/java/io/github/deeqma/music/Main.java
@@ -2,10 +2,7 @@ package io.github.deeqma.music;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
 @SpringBootApplication
 public class Main  {
 
@@ -13,8 +10,4 @@ public class Main  {
 		SpringApplication.run(Main.class, args);
 	}
 
-	@GetMapping("/")
-	public String h(){
-		return "musicApp";
-	}
 }
