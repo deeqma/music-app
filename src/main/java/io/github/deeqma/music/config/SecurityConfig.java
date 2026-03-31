65936abd1a9a4e20bf1ee1eeae8ad0075de8eeaf
stykle <Maxamadabdulahist@outlook.com>
2026-03-31 20:26:29 +0200

    made cors env

--- src/main/java/io/github/deeqma/music/config/SecurityConfig.java
+++ src/main/java/io/github/deeqma/music/config/SecurityConfig.java
@@ -32,6 +32,9 @@ public class SecurityConfig {
     @Value("${jwt.secret}")
     private String jwtSecret;
 
+    @Value("${cors.allowed-origins}")
+    private List<String> allowedOrigins;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http
@@ -54,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(allowedOrigins);
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of(
